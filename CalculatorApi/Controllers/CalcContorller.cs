940c9ce225337df89edd96ae1357f7e63aba26be
HanLin <3358454532@qq.com>
2026-04-29 21:04:03 +0800

    CalculatorApi

---
+++ CalculatorApi/Controllers/CalcContorller.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+namespace CalculatorApi.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class CalcController : ControllerBase
+{
+    [HttpGet("Add/{a}/{b}")]
+    public IActionResult Add(int a, int b) => Ok(a + b);
+    [HttpGet("Subtract/{a}/{b}")]
+    public IActionResult Subtract(int a, int b) => Ok(a - b);
+    [HttpGet("Multiply/{a}/{b}")]
+    public IActionResult Multiply(int a, int b) => Ok(a * b); [HttpGet("Divide/{a}/{b}")]
+    public IActionResult Divide(int a, int b)
+    {
+        if (b == 0) return BadRequest("除数不能为0");
+        return Ok((double)a / b);
+    }
+    [HttpGet("Leap/{year}")]
+    public IActionResult IsLeapYear(int year)
+    {
+        bool isLeap = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+        return Ok(new { Year = year, IsLeapYear = isLeap });
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿Console.WriteLine("");
+
+try
+{
+   
+    Console.Write("请输入第一个数：");
+    double num1 = double.Parse(Console.ReadLine()!); 
+
+    
+    Console.Write("请输入运算符（+ - * /）：");
+    string op = Console.ReadLine()!;
+
+    
+    Console.Write("请输入第二个数：");
+    double num2 = double.Parse(Console.ReadLine()!);
+
+    
+    double result;
+    bool isSuccess = true; 
+
+    switch (op)
+    {
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "*":
+            result = num1 * num2;
+            break;
+        case "/":
+           
+            if (num2 == 0)
+            {
+                Console.WriteLine("");
+                isSuccess = false;
+                break;
+            }
+            result = num1 / num2;
+            break;
+        default:
+            Console.WriteLine("");
+            isSuccess = false;
+            break;
+    }
+
+    if (isSuccess)
+    {
+        Console.WriteLine($" 计算结果：{num1} {op} {num2} = {result}");
+    }
+}
+catch (FormatException)
+{
+    Console.WriteLine("");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"{ex.Message}");
+}
+
+Console.WriteLine("");
+Console.ReadKey();
